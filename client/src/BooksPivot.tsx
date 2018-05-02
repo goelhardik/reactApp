876537227ac1ddb36f7be363735e72e60b63dd17
Hardik <hardik.iitr@gmail.com>
2018-05-01 23:14:07 -0700

    Minor change to pane

--- client/src/BooksPivot.tsx
+++ client/src/BooksPivot.tsx
@@ -13,59 +13,59 @@ export class BooksPivot extends React.Component {
                     />
                 </div>
                 <div className="rightPane">
-                This is the description of verse and verse itself. What's it look like
-                </div>
+                            This is the description of verse and verse itself. What's it look like
+                                </div>
             </div>
-        );
-    }
-
+                );
+            }
+        
     private getNavGroups() {
         return [
             {
+                    links: [
+                    {
+                    name: BookNames.Gita,
+                key: BookNames.Gita,
+                url: '',
                 links: [
-                    {
-                        name: BookNames.Gita,
-                        key: BookNames.Gita,
-                        url: '',
-                        links: [
                             {
-                                name: 'Child link',
-                                key: 'Child link 1',
-                                url: ''
-                            },
+                    name: 'Child link',
+                key: 'Child link 1',
+                url: ''
+            },
                             {
-                                name: 'Child link',
-                                key: 'Child link 2',
-                                url: '',
-                                links: [
+                    name: 'Child link',
+                key: 'Child link 2',
+                url: '',
+                links: [
                                     {
-                                        name: 'Child link',
-                                        key: 'Child link 3',
-                                        url: ''
-                                    }
-                                ]
-                            },
-                            {
-                                name: 'Child link',
-                                key: 'Child link 4',
-                                url: ''
-                            }
-                        ]
-                    },
-                    {
-                        name: BookNames.Aparokshanubhuti,
-                        key: BookNames.Aparokshanubhuti,
-                        url: '',
-                        links: [
-                            {
-                                name: 'Child link',
-                                key: 'Child link 5',
-                                url: ''
-                            },
-                        ]
-                    }
-                ]
+                    name: 'Child link',
+                key: 'Child link 3',
+                url: ''
             }
-        ];
+        ]
+    },
+                            {
+                    name: 'Child link',
+                key: 'Child link 4',
+                url: ''
+            }
+        ]
+    },
+                    {
+                    name: BookNames.Aparokshanubhuti,
+                key: BookNames.Aparokshanubhuti,
+                url: '',
+                links: [
+                            {
+                    name: 'Child link',
+                key: 'Child link 5',
+                url: ''
+            },
+        ]
     }
+]
+}
+];
+}
 }
